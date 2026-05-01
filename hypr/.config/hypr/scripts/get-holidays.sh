bc2aeaaa884e253d6b259aecc8fef487c988032e
saiakat <maxstender1@gmail.com>
2026-05-01 12:20:10 +0200

    Check if cache files exist

--- hypr/.config/hypr/scripts/get-holidays.sh
+++ hypr/.config/hypr/scripts/get-holidays.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+
+FILE_PATH="$HOME/.cache/waybar/holidays"
+if [ -e "$FILE_PATH" ]; then
+  echo "Already exists: $FILE_PATH"
+else
+  mkdir -p "$(dirname "$FILE_PATH")" && touch "$FILE_PATH"
+  echo "Created: $FILE_PATH"
+fi
 # 1. Path to the activation script
 source ~/utils/venv/bin/activate
 
 # 2. Run your script (it will now use the venv's python)
-python ~/utils/get_holidays.py > $HOME/.cache/waybar/holidays
+python ~/utils/get_holidays.py > $FILE_PATH
 
 # 3. Optional: deactivate when finished
 deactivate
--- hypr/.config/hypr/scripts/get-updates.sh
+++ hypr/.config/hypr/scripts/get-updates.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+FILE_PATH="$HOME/.cache/waybar/updates"
+if [ -e "$FILE_PATH" ]; then
+  echo "Already exists: $FILE_PATH"
+else
+  mkdir -p "$(dirname "$FILE_PATH")" && touch "$FILE_PATH"
+  echo "Created: $FILE_PATH"
+fi
+
 cache="$HOME/.cache/waybar/updates"
 updates=$(checkupdates | wc -l)
-echo $updates > $HOME/.cache/waybar/updates
+echo $updates > $FILE_PATH
