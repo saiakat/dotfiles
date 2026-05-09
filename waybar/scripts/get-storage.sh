25fc483822e1dda2cfdf107a222e56b5ebd596e8
saiakat <maxstender1@gmail.com>
2026-05-09 21:22:56 +0200

    added signal and sata1 to disks

--- waybar/scripts/get-storage.sh
+++ waybar/scripts/get-storage.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 cap_home=$(df -h /home | awk 'NR==2 {print $3 "/" $2}')
+cap_sata1=$(df -h /run/media/sata1 | awk 'NR==2 {print $3 "/" $2}')
 cap_root=$(df -h / | awk 'NR==2 {print $3 "/" $2}')
 
-echo "{\"text\": \" root: $cap_root\n home: $cap_home\", \"tooltip\": \"root: $cap_root\nhome: $cap_home\"}"
+echo "{\"text\": \" root: $cap_root\n home: $cap_home\n sata1: $cap_sata1\", \"tooltip\": \"root: $cap_root\nhome: $cap_home\nsata1: $cap_sata1\"}"
 
